963b8bb44e874d74233810e632fec06d259dde58
murugan cs <pm.murugancs@gmail.com>
2019-04-13 21:39:13 +0530

    initial commit

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\File;
+use Illuminate\Support\Facades\Storage;
+use Mail;
+
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    protected function create(array $data)
+    {
+		$path = Storage::putFile('avatars', $data['image']);
+
+		User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'profile_image' => 'name',
+        ]);
+		
+		$cv_path = storage_path(). '/app/'.$path;
+
+        Mail::send('tttte',   ['name' => $data['name']], 
+
+		function($message) use ($data, $path,$cv_path){
+		
+			$message->from($data['email']);
+			
+			$message->to($data['email'])->subject('Voucher');
+
+			$message->attach($cv_path);
+		});
+        /* return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]); */
+		print_r(5);die;
+		
+    }
+}
